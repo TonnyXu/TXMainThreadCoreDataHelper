5964d4313f8522c60299df549f5002ed01417b3b
Tonny Xu <tonny.xu@gmail.com>
2013-03-01 16:15:36 +0900

    Added static library project files

--- TXMainThreadMOC_Definition.h
+++ TXMainThreadMOC_Definition.h
@@ -5,7 +5,7 @@
 //  Copyright 2013 Tonny Xu. All rights reserved.
 //
 
-#if (1)
+#if (0)
   #define DBFileName @"CoreDataBooks.CDBStore"
 #else
   #define DBFileName @"<#Set your DB file's name here, like `myDB.sqlite`#>"
